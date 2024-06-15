cfb5c8f2042c011bb5cfd8b11852cf70d2041bae
MarKr17 <mariakroczek199@gmail.com>
2024-06-15 12:10:51 +0200

    adds antibody prodyction by plasma cells

--- Agents/Plasma_cell.py
+++ Agents/Plasma_cell.py
@@ -6,10 +6,13 @@ class Plasma_cell(Cell):
     def __init__(self, unique_id, model, proliferation_rate):
         super().__init__(unique_id, model, proliferation_rate)
         self.antigen_presented = ''
+        self.antibody_production_rate = 0
 
     def step(self):
         self.move()
         self.proliferation()
+        if self.antigen_presented != '':
+            self.antibody_production()
         if self.health <= 0:
             self.death()
 
@@ -21,4 +24,13 @@ class Plasma_cell(Cell):
             self.model.new_agents.append(n)
             self.tiredness += 1
 
-# def antibody_production(self):
+    def antibody_production(self):
+        r = random.randint(0, 99)
+        if r < self.antibody_production_rate:
+            x = self.pos[0]
+            y = self.pos[1]
+            if self.antigen_presented == "MBP":
+                self.model.MBP_antibody_matrix[x][y] += 1
+            if self.antigen_presented == "EBNA1":
+                self.model.EBNA1_antibody_matrix[x][y] += 1
+            self.tiredness += 1
