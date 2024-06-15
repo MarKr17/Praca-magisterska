32f57c029e2e7bd018060fb6619ec4fad7e17544
MarKr17 <mariakroczek199@gmail.com>
2024-06-15 13:04:38 +0200

    adds antigen attachment to APC

--- Agents/APC.py
+++ Agents/APC.py
@@ -7,10 +7,13 @@ class APC(Cell):
         super().__init__(unique_id, model, proliferation_rate)
         self.MHC_grow_rate = 5
         self.num_MHC = 0
+        self.antigen_attached = ''
 
     def step(self):
         self.move()
         self.proliferation()
+        if self.antigen_attached != '':
+            self.antigen_attachment()
         if self.health <= 0:
             self.death
 
@@ -18,3 +21,14 @@ class APC(Cell):
         r = random.randint(0, 99)
         if r < self.MHC_grow_rate:
             self.num_MHC += 1
+
+    def antigen_attachment(self):
+        MBP = self.model.MBP_matrix[self.pos[0], self.pos[1]]
+        EBNA1 = self.model.EBNA1_matrix[self.pos[0], self.pos[1]]
+        if MBP + EBNA1 != 0:
+            if MBP > EBNA1:
+                self.antigen_attached = "MBP"
+                self.model.MBP_matrix[self.pos[0], self.pos[1]] -= 1
+            else:
+                self.antigen_attached = "EBNA1"
+                self.model.EBNA1_matrix[self.pos[0], self.pos[1]] -= 1
