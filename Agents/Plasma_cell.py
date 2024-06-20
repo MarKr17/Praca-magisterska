79555b64a8c69463fbd9b1808b80db7c1446b14c
MarKr17 <mariakroczek199@gmail.com>
2024-06-20 17:02:20 +0200

    plasma cells only 1 action

--- Agents/Plasma_cell.py
+++ Agents/Plasma_cell.py
@@ -11,28 +11,26 @@ class Plasma_cell(Cell):
 
     def step(self):
         self.move()
-        self.proliferation()
-        if self.antigen_presented != '':
+        r = random.randint(0, 99)
+        if r < self.proliferation_rate:
+            self.proliferation()
+        if self.antigen_presented != '' and r < self.antibody_production_rate:
             self.antibody_production()
         self.calculate_dmg()
         if self.health <= 0:
             self.death()
 
     def proliferation(self):
-        r = random.randint(0, 99)
-        if r < self.proliferation_rate:
-            n = Plasma_cell(self.model.ID, self.model, self.proliferation_rate)
-            self.model.ID += 1
-            self.model.new_agents.append(n)
-            self.tiredness += 2
+        n = Plasma_cell(self.model.ID, self.model, self.proliferation_rate)
+        self.model.ID += 1
+        self.model.new_agents.append(n)
+        self.tiredness += 1
 
     def antibody_production(self):
-        r = random.randint(0, 99)
-        if r < self.antibody_production_rate:
-            x = self.pos[0]
-            y = self.pos[1]
-            if self.antigen_presented == "MBP":
-                self.model.MBP_antibody_matrix[x][y] += 1
-            if self.antigen_presented == "EBNA1":
-                self.model.EBNA1_antibody_matrix[x][y] += 1
-            self.tiredness += 1
+        x = self.pos[0]
+        y = self.pos[1]
+        if self.antigen_presented == "MBP":
+            self.model.MBP_antibody_matrix[x][y] += 1
+        if self.antigen_presented == "EBNA1":
+            self.model.EBNA1_antibody_matrix[x][y] += 1
+        self.tiredness += 1
